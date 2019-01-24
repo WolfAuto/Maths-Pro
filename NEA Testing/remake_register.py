73e517a232e3f93ca967137755ec076bc6aa9e8f
Kymani McGregor <33226271+WolfAuto@users.noreply.github.com>
2019-01-24 20:18:48 +0000

    Using loose coupling

---
+++ NEA Testing/remake_register.py
@@ -0,0 +1,150 @@
+import sqlite3 as sql  # module for database connection
+from tkinter import messagebox  # module for error messages on the tkinter page
+import string
+with sql.connect("newfile.db") as db:  # sets the connection to tbe database file
+    global cursor  # makes the cursor a global variable for all parts of the program
+    cursor = db.cursor()  # sets the cursor to allow sql statement execution
+
+create_student_table = ("""CREATE TABLE IF NOT EXISTS students(account_id INTEGER PRIMARY KEY,
+                        firstname VARCHAR(30) NOT NULL,
+                        surname VARCHAR(30) NOT NULL,  age INTEGER NOT NULL,
+                        class VARCHAR (3) NOT NULL, gender VARCHAR (30) NOT NULL,
+                         username VARCHAR(30),password VARCHAR(30), email VARCHAR(30))""")
+
+create_teacher_table = ("""CREATE TABLE IF NOT EXISTS teachers(account_id INTEGER PRIMARY KEY,
+                        firstname VARCHAR(30) NOT NULL,
+                        surname VARCHAR(30) NOT NULL,  age INTEGER NOT NULL,
+                        class VARCHAR (3) NOT NULL, gender VARCHAR (30) NOT NULL,
+                         username VARCHAR(30), password VARCHAR(30), email VARCHAR(30))""")
+# Sql statment to create the table where the user information will be stored
+cursor.execute(create_student_table)  # executes the sql statement
+
+# Sql statment to create the table where the user information will be stored
+cursor.execute(create_teacher_table)  # executes the sql statement
+db.commit()  # saves changes made to the sql file
+
+
+def register1(firstname, surname, age, school_class, var, var1):  # Function for registration
+    Age = int(age)  # ensures the age is a number
+    if var == 1:  # changing the var to a gender either male or female depending on value
+        gender = "Male"
+    elif var == 2:
+        gender = "Female"
+    if var1 == 1:  # changin the var1 to a school either student or teacher depending on value
+        insert_details = (
+            """INSERT INTO students(firstname, surname, age, class, gender)
+            VALUES(?, ?, ?, ?, ?)""")  # sql statement to insert a record into table students
+        cursor.execute(insert_details, [(firstname), (surname),
+                                        (Age), (school_class), (gender)])  # execution of sql statement
+    elif var1 == 2:
+        insert_details = (
+            """INSERT INTO teachers(firstname, surname, age, class, gender)
+            VALUES(?, ?, ?, ?, ?)""")  # sql statement to insert a record into table teacher
+        cursor.execute(insert_details, [(firstname), (surname),
+                                        (Age), (school_class), (gender)])  # execution of sql statement
+    print(firstname, surname, Age, school_class,
+          gender)  # for testing purposes
+    # db.commit()  # saves changes made to the database file
+    return True  # this is to ensure the record has been made to move to the next part of the registration
+
+
+def username_check(username):  # function for username vaildation
+    # sql statement for checking existing users
+    existing_users = cursor.execute(
+        "SELECT * from student,teacher where username = ?", (username,))
+
+    checking = cursor.fetchall()  # stores the result of sql search
+    if checking:  # if a username matches the username typed it returns false
+        messagebox.showerror(
+            "Error", "That username has already been taken please try another username")  # tkinter error message
+        return False
+    else:
+        return True
+
+
+def password_check(password, password_confirm):  # function for password vaildation
+    if password == password_confirm:
+        if len(password) >= 8:  # checks whether the password length is 8 chracterslong
+            # checks for letters in the password
+            if len(set(string.ascii_lowercase).intersection(password)) > 0:
+                # checks for numbers or special characters in the password
+                if (len(set(string.ascii_uppercase).intersection(password)) > 0):
+                    # checks for uppercase characters
+                    if (len(set(string.ascii_digit).intersection(password))) > 0:
+                        if (len(set(string.ascii_punctuation).intersection(password))) > 0:
+                            if password == password_confirm:  # ensures that the two password variables match
+                                return True
+                            else:
+                                messagebox.showwarning(
+                                    "Password", "Passwords don't match re enter confirm password again")
+                                # tkinter error message
+                                return False
+                        else:
+                            messagebox.showwarning(
+                                "Password", "Password doesn't contain a special character")
+                            return False
+                    else:
+                        # tkinter error message
+                        messagebox.showwarning(
+                            "Password", "Password don't contain digits character")
+                        return False
+                else:
+                    messagebox.showwarning(
+                        "Password", "Password don't contain any uppercase characters")  # tkinter error message
+                    return False
+            else:
+                messagebox.showwarning(
+                    "Password", "Password don't contain any lowercase letters")  # tkinter error message
+            return False
+        else:
+            messagebox.showwarning(
+                "Password", "Password is not 8 characters long")  # tkinter error message
+            return False
+    else:
+        messagebox.showwarning(
+            "Password", "Password don't match")  # tkinter error message
+        return False
+
+
+def email_check(email):  # function for email vaildation
+    if email.find("@") == -1:  # checks for a @ sign
+        return False
+    elif email.find(".") < (email.find("@") + 2):  # checks for a . and ensures that
+        return False
+    elif email.find(" ") != -1:  # ensures there are no spaces
+        return False
+    else:
+        return True
+
+
+# function for entering a record into accounts
+def register2(username, password, confirm_password, email, var1):
+    # checks whether a existing username with the username enter exists
+    if username_check(username):
+        # ensures the password passes all the vaildations
+        if password_check(password, confirm_password):
+            if email_check(email):  # ensures the email passes the vaildation
+                if var1 == 1:
+                    insert_student = (
+                        "INSERT INTO students(username,password,email) VALUES (?, ?, ?)")
+                    cursor.execute(insert_student, [
+                                   (username), (password), (email,)])
+                elif var1 == 2:
+                    insert_teacher = (
+                        "INSERT INTO teachers(username,password,email) VALUES (?, ?, ?)")
+                    cursor.execute(insert_student, [
+                                   (username), (password), (email)])
+
+                db.commit()  # saves the changes to the database file
+                db.close()  # closes the connection to the database file
+                return True
+            else:
+                messagebox.showwarning("Warning Email Address",
+                                       "Please enter a vaild email address")  # tkinter error message
+                return False
+        else:
+            return False
+    else:
+        messagebox.showwarning(
+            "Warning Username", "That username is already taken please try another one")  # tkinter error message
+        return False
--- NEA Testing/test 2 (using grid).py
+++ NEA Testing/test 2 (using grid).py
@@ -1,10 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
-from register_backend import register1, register2
+# this is import the functionailty of the registration from another python file
+# from register_backend import register1, register2
+from remake_register import register1, register2
 title_font = ("Times New Roman", 50)  # Setting font for titles on the frames
 large_font = ("Times New Roman", 24)  # Setting fonts for main headings
 medium_font = ("Times New Roman", 16)  # Setting fonts for text that will appear on the screen
 small_font = ("Times New Roman", 12)  # Setting fonts for small deails to be displayed
+
+# http://inloop.github.io/sqlite-viewer/ for checking the database file https://sqliteonline.com/
 
 
 class MathsPro(tk.Tk):
@@ -12,6 +16,15 @@ class MathsPro(tk.Tk):
     def __init__(self, *args, **kwargs):  # Creating a class that inherits from tk.Tk
 
         tk.Tk.__init__(self, *args, **kwargs)  # intialises the object
+        self.shared_data = {"firstname": tk.StringVar(),
+                            "surname": tk.StringVar(),
+                            "Age": tk.StringVar(),
+                            "Class": tk.StringVar(),
+                            "var": tk.IntVar(),
+                            "var1": tk.IntVar(),
+                            "username": tk.StringVar(),
+                            "password": tk.StringVar(),
+                            "email": tk.StringVar()}
 
         tk.Tk.wm_title(self, "Maths Pro")  # Sets the title of each page to be Maths Pro
         container = tk.Frame(self)  # defined a container for all the frame be kept
@@ -63,72 +76,76 @@ class Register(tk.Frame):  # Creating a class that inheirts tk.Frame from tkinte
 
         # Label for where the user enters their first name
         label_1 = tk.Label(self, text="First Name", font=small_font)
-        label_1.grid(row=1, column=0, pady=20)
+        label_1.grid(row=1, column=0, pady=20
 
-        firstname = tk.StringVar()  # Stores the actual firstname the user types in
-
-        entry_1 = tk.Entry(self, textvariable=firstname)  # Where the user enter their first name
+        # Where the user enter their first name
+        entry_1=tk.Entry(self, textvariable=self.controller.shared_data["firstname"])
         entry_1.grid(row=1, column=1)
 
         # Label for where the user enters their surname
-        label_a = tk.Label(self, text="Surname", font=small_font)
+        label_a=tk.Label(self, text="Surname", font=small_font)
         label_a.grid(row=2, column=0, pady=20)
 
-        surname = tk.StringVar()  # Stores the actual surname the user types in
 
-        entry_2 = tk.Entry(self, textvariable=surname)  # Entry of the surname of user
+
+        # Entry of the surname of user
+        entry_2=tk.Entry(self, textvariable=self.controller.shared_data["surnname"])
         entry_2.grid(row=2, column=1)
 
         # Label for where the user enters their age
-        label_b = tk.Label(self, text="Age", font=small_font)
+        label_b=tk.Label(self, text="Age", font=small_font)
         label_b.grid(row=3, column=0, pady=20)
 
-        age = tk.StringVar()  # Stores the actual age the user types in
 
-        entry_b = tk.Entry(self, textvariable=age)  # Entry for the Age of the student/teacher
+
+        # Entry for the Age of the student/teacher
+        entry_b=tk.Entry(self, textvariable=self.controller.shared_data["age"])
         entry_b.grid(row=3, column=1)
 
         # Label for where the user enters their class
-        label_c = tk.Label(self, text="Class", font=small_font)
+        label_c=tk.Label(self, text="Class", font=small_font)
         label_c.grid(row=4, column=0, pady=20)
 
-        school_class = tk.StringVar()  # Stores the class which the user types in
+
 
         # Entry for the class in which the student/teacher is apart of
-        entry_c = tk.Entry(self, textvariable=school_class)
+        entry_c=tk.Entry(self, textvariable=self.controller.shared_data["Class"])
         entry_c.grid(row=4, column=1)
 
         # Where the user enters their gender
-        label_2 = tk.Label(self, text="Gender", font=small_font)
+        label_2=tk.Label(self, text="Gender", font=small_font)
         label_2.grid(row=5, column=0, pady=20)
-        var = tk.IntVar()  # Stores the choice of the user for the gender option
 
-        tk.Radiobutton(self, text="Male", padx=5, variable=var, value=1).grid(
+
+        tk.Radiobutton(self, text="Male", padx=5, variable=self.controller.shared_data["var"], value=1).grid(
             row=5, column=1)  # The options for the user gender male
-        tk.Radiobutton(self, text="Female", padx=20, variable=var, value=2).grid(
+        tk.Radiobutton(self, text="Female", padx=20, variable=self.controller.shared_data["var"], value=2).grid(
             row=5, column=2)  # the options for the user gender female
 
-        label_3 = tk.Label(self, text="School", width=20, font=small_font)  # School of the user
+        label_3=tk.Label(self, text="School", width=20, font=small_font)  # School of the user
         label_3.grid(row=6, column=0, pady=20)
 
-        var1 = tk.IntVar()  # Stores the choice of the user for the school option
-        tk.Radiobutton(self, text="Student", padx=5, variable=var1, value=1).grid(
+
+        tk.Radiobutton(self, text="Student", padx=5, variable=self.controller.shared_data["var1"], value=1).grid(
             row=6, column=1)  # Option for the user either student
-        tk.Radiobutton(self, text="Teacher", padx=20, variable=var1, value=2).grid(
+        tk.Radiobutton(self, text="Teacher", padx=20, variable=self.controller.shared_data["var1"], value=2).grid(
             row=6, column=2)  # Option for the user either teacher
-        button1 = tk.Button(self, text="Enter details",
-                            command=lambda: self.register(controller, firstname.get(), surname.get(), int(age.get()),
-                                                          school_class.get(), var.get(), var1.get()))
+        firstname=self.controller.shared_data["firstname"].get()
+        surname=self.controller.shared_data["surname"].get()
+        age=self.controller.shared_data["Age"].get()
+        school_class=self.controller.shared_data["Class"].get()
+        var=self.controller.shared_data["var"].get()
+        var1=self.controller.shared_data["var1"].get()
+        button1=tk.Button(self, text="Enter details",
+                            command=lambda: self.register(controller, firstname, surname, int(age),
+                                                          school_class, var, var1)
         # Runs the register function in the other python file
         button1.grid(row=7, column=9)
 
-        button2 = tk.Button(self, text="Check details")
-        button2.grid(row=7, column=0)
-
-        def register(self, controller, firstname, surname, age, school_class, var, var1):  # function used changing frames
-            # vaildation of all the personal deatils and these details being added into the record
-            if register1(firstname, surname, age, school_class, var, var1) == True:
-                controller.show_frame(Register2)  # switches to the next part of the registration
+    def register(self, controller, firstname, surname, age, school_class, var, var1):  # function used changing frames
+        # vaildation of all the personal deatils and these details being added into the record
+        if register1(firstname, surname, age, school_class, var, var1) == True:
+            controller.show_frame(Register2)  # switches to the next part of the registration
 
 
 class Register2(tk.Frame):
@@ -137,62 +154,62 @@ class Register2(tk.Frame):
         # Changes the background of the colour to be grey (allows for the specific frame to be styled)
         tk.Frame.config(self, bg="grey")
         # Title Page of the second part of the Registration Form
-        label = tk.Label(self, text="Register 2", font=title_font, bg="grey")
+        label=tk.Label(self, text="Register 2", font=title_font, bg="grey")
         label.grid(row=0, column=0)
 
-        separator = ttk.Separator(self, orient="vertical")
+        separator=ttk.Separator(self, orient="vertical")
         separator.grid(row=1, column=3, rowspan=7, sticky="ns")
 
-        guide = "Enter a username more than 8 characters long and password that is 8 characters long \n contains uppercase, lowercase characters, numeric and special characters"
-        label_text = tk.Label(self, text=guide, font=small_font, bg="grey")
+        guide="Enter a username more than 8 characters long and password that is 8 characters long \n contains uppercase, lowercase characters, numeric and special characters"
+        label_text=tk.Label(self, text=guide, font=small_font, bg="grey")
         label_text.grid(row=2, column=4)
 
         # Label for where the user enters their username
-        label_0 = tk.Label(self, text="Username", font=small_font, bg="grey")
+        label_0=tk.Label(self, text="Username", font=small_font, bg="grey")
         label_0.grid(row=1, column=0, pady=20)
 
-        username_entry = tk.StringVar()
-        entry_0 = tk.Entry(self, textvariable=username_entry)
+        username_entry=tk.StringVar()
+        entry_0=tk.Entry(self, textvariable=username_entry)
         entry_0.grid(row=1, column=1)
 
         # Label for where the user enters their username
-        label_1 = tk.Label(self, text="Password", bg="grey")
+        label_1=tk.Label(self, text="Password", bg="grey")
         label_1.grid(row=2, column=0, pady=20)
 
-        password_entry = tk.StringVar()  # Stores the actual password the user password
+        password_entry=tk.StringVar()  # Stores the actual password the user password
 
         # Hides the user password when being typed in
-        entry_1 = tk.Entry(self, textvariable=password_entry, show="*")
+        entry_1=tk.Entry(self, textvariable=password_entry, show="*")
         entry_1.grid(row=2, column=1)
 
         # Label for where the user confirms the password they typed
-        label_2 = tk.Label(self, text="Confirm Password", bg="grey")
+        label_2=tk.Label(self, text="Confirm Password", bg="grey")
         label_2.grid(row=3, column=0, pady=20)
 
-        password_confirm = tk.StringVar()  # Stores the actual password the user password again (used for comparison)
+        confirm_password=tk.StringVar()  # Stores the actual password the user password again (used for comparison)
 
         # Hides the user password when being typed in
-        entry_2 = tk.Entry(self, textvariable=password_confirm, show="*")
+        entry_2=tk.Entry(self, textvariable=confirm_password, show="*")
         entry_2.grid(row=3, column=1)
 
         # Label for where the user enters their email address
-        label_3 = tk.Label(self, text="Email", bg="grey")
+        label_3=tk.Label(self, text="Email", bg="grey")
         label_3.grid(row=4, column=0, pady=20)
 
-        email_entry = tk.StringVar()
-        entry_3 = tk.Entry(self, textvariable=email_entry)
+        email_entry=tk.StringVar()
+        entry_3=tk.Entry(self, textvariable=email_entry)
         entry_3.grid(row=4, column=1)
-        button1 = ttk.Button(self, text="Back to home",
+        button1=ttk.Button(self, text="Back to home",
                              command=lambda: controller.show_frame(Register))
         button1.grid(row=5, column=0)
 
-        button2 = ttk.Button(self, text="Create Account", command=lambda: self.register(
-            username_entry.get(), password_entry.get(), password_confirm.get(), email_entry.get()))
+        button2=ttk.Button(self, text="Create Account", command=lambda: self.register(controller,
+                                                                                        username_entry.get(), password_entry.get(), confirm_password.get(), email_entry.get(), var1.get()))
         button2.grid(row=6, column=2)
 
-        def register(self, controller, username, password, password_confirm, email):
-            if register2(username, password, password_confirm, email) == True:
-                controller.show_frame(Main_Menu)
+    def register(self, controller, username, password, password_confirm, email, var1):
+        if register2(username, password, password_confirm, email, var1) == True:
+            controller.show_frame(Main_Menu)
 
 
 class Main_Menu(tk.Frame):
@@ -200,26 +217,26 @@ class Main_Menu(tk.Frame):
         tk.Frame.__init__(self, parent)
         tk.Frame.config(self, bg="blue")
 
-        text_label = tk.Label(self, text="Maths Pro", font=title_font)
+        text_label=tk.Label(self, text="Maths Pro", font=title_font)
         text_label.pack(padx=20, pady=20)
 
-        label_1 = tk.Label(self, text="Username", font=medium_font)
+        label_1=tk.Label(self, text="Username", font=medium_font)
         label_1.pack()
 
-        username = tk.StringVar()
+        username=tk.StringVar()
 
-        entry_1 = tk.Entry(self, textvariable=username)
+        entry_1=tk.Entry(self, textvariable=username)
         entry_1.pack()
 
-        label_2 = tk.Label(self, text="Password", font=medium_font)
+        label_2=tk.Label(self, text="Password", font=medium_font)
         label_2.pack()
 
-        password = tk.StringVar()
+        password=tk.StringVar()
 
-        entry_2 = tk.Entry(self, textvariable=password)
+        entry_2=tk.Entry(self, textvariable=password)
         entry_2.pack()
 
 
-root = MathsPro()  # this runs the Maths Pro class
+root=MathsPro()  # this runs the Maths Pro class
 root.geometry("1280x800")  # changes the size of the window
 root.mainloop()  # As MathsPro inherited from tkinter this function can be moved

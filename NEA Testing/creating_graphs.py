228503fa7d99452ea33d450edc5301a7d04aed15
Kymani McGregor <33226271+WolfAuto@users.noreply.github.com>
2019-03-26 22:31:24 +0000

    Creating Graphs
    Code needed to create graphs using lists

---
+++ NEA Testing/creating_graphs.py
@@ -0,0 +1,30 @@
+from create_connection import cursor,cursor1,db
+
+def graph_pure(user_id):
+    global result
+    result = []
+    dates = []
+    correct = []
+    incorrect = []
+    total_questions = []
+    sql = "SELECT time_stamp,correct_pure,incorrect_pure,total_questions FROM pure_results WHERE user_id = ? ORDER BY time_stamp"
+    cursor.execute(sql, [(user_id)])
+    for row in cursor.fetchall():
+        result.append(row)
+    for data in range(0,len(result)-1):
+        if result[data][0] == result[data+1][0]:
+            print("same date")
+        else:
+            print("pass")
+def graph_correct_pure(user_id):
+    result = []
+    dates = []
+    correct = []
+    sql = "SELECT time_stamp, correct_pure, FROM pure_results WHERE user_id = ? "
+    cursor.execute(sql , [(user_id)])
+    for row in cursor.fetchall():
+        result.append(row)
+    for data in range(0, len(result)-1):
+        if result[data][0] == result[data+1][0]
+        
+        

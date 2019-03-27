0e8f778b69fcef99538bb924782523b9f9aeb506
Kymani McGregor <33226271+WolfAuto@users.noreply.github.com>
2019-03-27 21:36:52 +0000

    Added a notepad file and edited graphing function
    Trying to figure out how to graph sqlite3 data

--- NEA Testing/creating_graphs.py
+++ NEA Testing/creating_graphs.py
@@ -1,4 +1,5 @@
-from create_connection import cursor,cursor1,db
+from create_connection import cursor, cursor1, db
+
 
 def graph_pure(user_id):
     global result
@@ -11,20 +12,37 @@ def graph_pure(user_id):
     cursor.execute(sql, [(user_id)])
     for row in cursor.fetchall():
         result.append(row)
-    for data in range(0,len(result)-1):
+    for data in range(0, len(result)-1):
         if result[data][0] == result[data+1][0]:
             print("same date")
         else:
             print("pass")
+
+
 def graph_correct_pure(user_id):
+    global result
     result = []
     dates = []
     correct = []
-    sql = "SELECT time_stamp, correct_pure, FROM pure_results WHERE user_id = ? "
-    cursor.execute(sql , [(user_id)])
+    sql = "SELECT time_stamp, correct_pure FROM pure_results WHERE user_id = ? ORDER BY time_stamp"
+    value = 0
+    cursor.execute(sql, [(user_id)])
     for row in cursor.fetchall():
         result.append(row)
     for data in range(0, len(result)-1):
-        if result[data][0] == result[data+1][0]
-        
-        
+        if result[data][0] == result[data+1][0]:
+            value = value + (result[data][1] + result[data+1][1])
+            if dates.find(result[data][0]) == -1:
+                dates.append(result[data][0])
+            else:
+                print("no duplicates")
+        else:
+            correct.append(value)
+            value = 0
+    print("Dates")
+    print(dates)
+    print("Correct")
+    print(correct)
+
+
+graph_correct_pure(8)
